7759dd80c18ff952a66c107e65256eff7667eaf6
unknown <noahhusby@gmail.com>
2019-11-21 17:27:38 -0500

    Repaired instance method

--- solenoid.py
+++ solenoid.py
@@ -1,13 +1,15 @@
 class solenoid:
-    def __init__(self,id):
+    def __init__(self, id):
         self.id = id
 
     try:
         import RPi.GPIO as GPIO
     except RuntimeError:
         print("Error importing RPi.GPIO!  This is probably because you need superuser privileges.  You can achieve this by using 'sudo' to run your script")
-    GPIO.setmode(GPIO.BOARD)
-    GPIO.setup(self.id, GPIO.OUT, initial=GPIO.LOW)
+    
+    def init():
+        GPIO.setmode(GPIO.BOARD)
+        GPIO.setup(self.id, GPIO.OUT, initial=GPIO.LOW)
 
     def isEnabled():
         return GPIO.input(self.id)
